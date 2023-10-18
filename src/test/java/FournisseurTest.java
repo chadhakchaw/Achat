5d219c4fdbc68e1a363ad727b9ab50c88e604624
khali098 <103378775+khali098@users.noreply.github.com>
2023-10-18 09:27:17 +0100

    test

--- src/test/java/FournisseurTest.java
+++ src/test/java/FournisseurTest.java
@@ -1,46 +1,47 @@
-import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
 import org.junit.Before;
 import org.junit.Test;
+import tn.esprit.rh.achat.entities.CategorieFournisseur;
+import tn.esprit.rh.achat.entities.DetailFournisseur;
 import tn.esprit.rh.achat.entities.Fournisseur;
+
+import java.util.Date;
+
 import static org.junit.Assert.*;
-import static org.mockito.Mockito.*;
 
 public class FournisseurTest {
-
     private Fournisseur fournisseur;
 
     @Before
     public void setUp() {
-        // Initialisation avant chaque test
         fournisseur = new Fournisseur();
-    }
-
-    @Test
-    public void testGetIdFournisseur() {
-        // Définir l'ID du fournisseur
         fournisseur.setIdFournisseur(1L);
-
-        // Vérifier si l'ID est correct
-        assertEquals(1L, fournisseur.getIdFournisseur());
-    }
-
-    @Test
-    public void testGetCode() {
-        // Définir le code du fournisseur
-        fournisseur.setCode("F123");
-
-        // Vérifier si le code est correct
-        assertEquals("F123", fournisseur.getCode());
-    }
-
-    @Test
-    public void testGetLibelle() {
-        // Définir le libellé du fournisseur
+        fournisseur.setCode("ABC123");
         fournisseur.setLibelle("Fournisseur ABC");
-
-        // Vérifier si le libellé est correct
-        assertEquals("Fournisseur ABC", fournisseur.getLibelle());
+        fournisseur.setCategorieFournisseur(CategorieFournisseur.ORDINAIRE);
     }
 
-    // Vous pouvez ajouter d'autres tests pour les autres attributs et méthodes de la classe Fournisseur
+    @Test
+    public void testFournisseurGetterSetter() {
+        assertEquals(Long.valueOf(1L), fournisseur.getIdFournisseur());
+        assertEquals("ABC123", fournisseur.getCode());
+        assertEquals("Fournisseur ABC", fournisseur.getLibelle());
+        assertEquals(CategorieFournisseur.ORDINAIRE, fournisseur.getCategorieFournisseur());
+    }
+
+    @Test
+    public void testFournisseurDetails() {
+        DetailFournisseur detailFournisseur = new DetailFournisseur();
+        detailFournisseur.setEmail("fournisseur@abc.com");
+        detailFournisseur.setDateDebutCollaboration(new Date());
+        detailFournisseur.setAdresse("123 Rue ABC");
+        detailFournisseur.setMatricule("MAT123");
+        fournisseur.setDetailFournisseur(detailFournisseur);
+
+        assertEquals("fournisseur@abc.com", fournisseur.getDetailFournisseur().getEmail());
+        assertNotNull(fournisseur.getDetailFournisseur().getDateDebutCollaboration());
+        assertEquals("123 Rue ABC", fournisseur.getDetailFournisseur().getAdresse());
+        assertEquals("MAT123", fournisseur.getDetailFournisseur().getMatricule());
+    }
 }
