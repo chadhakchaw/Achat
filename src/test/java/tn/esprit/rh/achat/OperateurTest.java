ecf340b6f86924c06fe6600b85d74ba0eac5bcd4
chadha <chadha.kchaw@esprit.tn>
2023-11-14 17:28:30 +0100

    test

--- src/test/java/tn/esprit/rh/achat/OperateurTest.java
+++ src/test/java/tn/esprit/rh/achat/OperateurTest.java
@@ -2,13 +2,28 @@ package tn.esprit.rh.achat;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import tn.esprit.rh.achat.entities.Operateur;
 import tn.esprit.rh.achat.entities.Reglement;
 import tn.esprit.rh.achat.entities.Facture;
+import tn.esprit.rh.achat.repositories.OperateurRepository;
+import tn.esprit.rh.achat.services.IOperateurService;
+import tn.esprit.rh.achat.services.OperateurServiceImpl;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class OperateurTest {
     private Operateur operateur;
+
+    @InjectMocks
+    private OperateurServiceImpl operateurService;
+    @Mock
+    private OperateurRepository operateurRepository;
 //test
     @Before
     public void setUp() {
@@ -33,6 +48,27 @@ public class OperateurTest {
         facture.setMontantRemise(5.0f);
         facture.setArchivee(true);
         assertEquals(facture, operateur.getFactures());
+    }
+
+    @Test
+    public void testAddOperateur() {
+        Operateur newOperateur = new Operateur();
+        newOperateur.setIdOperateur(2L);
+        newOperateur.setNom("ben abdallah");
+        newOperateur.setPrenom("thouraya");
+        newOperateur.setPassword("0000");
+        newOperateur.setFactures(operateur.getFactures());
+
+        when(operateurRepository.save(newOperateur)).thenReturn(newOperateur);
+
+        Operateur addOperateur = operateurService.addOperateur(newOperateur);
+
+        assertNotNull(addOperateur);
+        assertEquals(newOperateur.getIdOperateur(), addOperateur.getIdOperateur());
+        assertEquals(newOperateur.getNom(), addOperateur.getNom(), "kchaou");
+        assertEquals(newOperateur.getPrenom(), addOperateur.getPrenom(), "chacha");
+        assertEquals(newOperateur.getPassword(), addOperateur.getPassword(), "new");
+        verify(operateurRepository).save(newOperateur);
     }
 
 }
